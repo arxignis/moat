b2dd3e8b73082dfb4742ff123007b4f251565f94
David Papp <pappdav@gmail.com>
2025-10-30 16:02:11 +0100

    feat: enhance install script to include ClamAV setup and update dependency installation

--- install.sh
+++ install.sh
@@ -1,11 +1,23 @@
 #!/bin/sh
 
 VERSION=0.0.6
+BRANCH=main
 
 # Install dependencies
 echo "Installing dependencies..."
 apt-get update -qq
-apt-get install -yqq curl sed
+apt-get install -yqq curl sed clamav-daemon redis-server
+
+# Setup ClamAV
+
+echo "Setting up ClamAV..."
+curl -fSL https://raw.githubusercontent.com/arxignis/moat/refs/heads/${BRANCH}/others/clamd/clamd.conf -o /etc/clamd.d/scan.conf
+curl -fSL https://raw.githubusercontent.com/arxignis/moat/refs/heads/${BRANCH}/others/systemd/clamd/override.conf -o /etc/systemd/system/clamav-daemon.socket.d/override.conf
+systemctl daemon-reload
+systemctl restart clamav-daemon
+
+echo "ClamAV setup complete."
+sleep 1
 
 # Install moat
 echo "Installing moat ${VERSION} for $(arch)..."
@@ -14,7 +26,7 @@ tar -C /usr/local/bin -xzf /tmp/moat-$(arch)-unknown-linux-gnu.tar.gz
 
 # Install service
 echo "Installing service..."
-curl -fSL https://raw.githubusercontent.com/arxignis/moat/refs/heads/main/others/systemd/moat.service -o /etc/systemd/system/moat.service
+curl -fSL https://raw.githubusercontent.com/arxignis/moat/refs/heads/${BRANCH}/others/systemd/moat.service -o /etc/systemd/system/moat.service
 systemctl daemon-reload
 
 # Create directories
@@ -23,7 +35,7 @@ mkdir -p /var/log/moat /var/run/moat /var/lib/moat /etc/moat
 
 # Create config file
 echo "Creating config file..."
-curl -fSL https://raw.githubusercontent.com/arxignis/moat/refs/heads/main/config_example.yaml -o /etc/moat/config.yaml
+curl -fSL https://raw.githubusercontent.com/arxignis/moat/refs/heads/${BRANCH}/config_example.yaml -o /etc/moat/config.yaml
 chmod 644 /etc/moat/config.yaml
 
 # Enable and start service
@@ -31,5 +43,7 @@ echo "Enabling and starting service..."
 systemctl enable moat
 
 echo "Before starting the service, you need to add your API key to the config file /etc/moat/config.yaml."
+sleep 1
 echo "You can get your API key from https://dash.arxignis.com/settings/api-keys."
+sleep 1
 echo "Then run 'systemctl start moat' to start the service."
